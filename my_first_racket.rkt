4ac4f16d5ca4213ddb786d7d2b1f9a0ec287faec
Ben Higginbotham <bicycleben88@gmail.com>
2025-04-14 21:44:52 -0400

    complete questions 1 and 2

---
+++ my_first_racket.rkt
@@ -0,0 +1,34 @@
+;;;
+; CSE240 Functional Programming 1 Homework
+; This assignment involves pracicing various expressions and procedures in Scheme.
+; 
+; For the first assignment, we are providing the default header so you can see how it
+; looks in Scheme. For future assignments, you will need to add it yourself.
+;
+; Completion time: (fill in)
+;
+; @author (your name here)
+; @version (fill in)
+;;;
+
+
+; Q1
+
+
+; Q2
+
+
+; Q3
+
+ 
+; Q4
+
+
+; Q5
+
+
+; Q6
+
+
+; Q7
+
