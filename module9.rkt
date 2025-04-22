02dfa120b9e190a86d53df4d8db6bd9a89f5c6d8
Ben Higginbotham <bicycleben88@gmail.com>
2025-04-21 21:07:12 -0400

    questions 2 3

--- module9.rkt
+++ module9.rkt
@@ -16,19 +16,21 @@
         (else (sumEven (cdr list)))))
 
 ((lambda (lst)
-   (let ((sum 0) (rest-of-list (cdr lst)))
-     (+ sum (car lst)))) '(1 3 -4 5 6 -7))
-
-;(sumEven '(1 3 -4 5 6 -7))
+   (let add ((sum 0) (remains lst))
+     (cond
+       ((null? remains) sum)
+       ((even? (car remains))
+        (add (+ (car remains) sum) (cdr remains)))
+       (else (add sum (cdr remains)))))
+   )'(1 3 -4 5 6 -7))
 
 ; Q2
-;(define (list-push-back lst new-list)
-;  (append (lst new-list)))
-
-;(list-push-back '(4 3 7 1 2 9) '(1 2))
+(define (list-push-back lst new-list)
+                        (append lst new-list))
 
 ; Q3
-
+(define (list-push-front lst new-list)
+                        (append new-list lst))
  
 ; Q4
 
@@ -37,17 +39,3 @@
 
 
 ; Q6
-
-;1. Create an anonymous function that takes one argument: the input list.
-;2. Inside, set up a helper called “loop” with two variables:
-;remaining = the list you still need to process
-;sum = the running total of even numbers, start at 0
-;3. Loop (recurse) until remaining is empty:
-;If remaining is empty, return sum.
-;Otherwise, let x = the first element of remaining.
-;If x is even, add x to sum.
-;Remove the first element from remaining.
-;Call “loop” again with the new remaining and updated sum.
-;4. Finally, invoke your anonymous function on the literal list '(1 3 -4 5 6 -7).
-
-;When you follow those steps, you’ll get 2 (because –4 + 6 = 2).
